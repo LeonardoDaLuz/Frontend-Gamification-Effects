a41d5b776b54a24cb60357df079594d633bfaf94
Leonardo a Luz <leoluzprog@gmail.com>
2021-03-16 16:02:06 -0300

    backup

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: './js/main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ]
+}
